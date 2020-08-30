b691a1181947fc7e00186c3dd45a8f694e78b265
shayy <shay.yosef@forescout.com>
2020-08-30 16:35:24 +0300

    ccomoot

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,5 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello, World of Java");
-		System.out.println("This commit should fix previous issue");
     }
 }
